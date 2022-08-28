a2473f69ded6cb87852afe8203c1ce9317e50082
Apple <riyabulia12@gmail.com>
2022-08-28 20:31:56 +0530

    Add linters to project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
+import './main.css';
 
-import './main.css'
 const moment = require('moment');
 
 const localDate = document.getElementById('localDate');
 const localTime = document.getElementById('localTime');
 const utcDate = document.getElementById('utcDate');
-const utc = document.getElementById('utc');
 const utcTime = document.getElementById('utcTime');
 
 const updateTime = () => {
-   localDate.innerHTML = moment().format("ddd, MMM D YYYY");
-   utcDate.innerHTML = moment.utc().add(1, 'hours').format("ddd, MMM D YYYY");
-   localTime.innerHTML = moment().format("HH:mm:ss")
-   utcTime.innerHTML = moment.utc().add(1, 'hours').format('HH:mm:ss');
+  localDate.innerHTML = moment().format('ddd, MMM D YYYY');
+  utcDate.innerHTML = moment.utc().add(1, 'hours').format('ddd, MMM D YYYY');
+  localTime.innerHTML = moment().format('HH:mm:ss');
+  utcTime.innerHTML = moment.utc().add(1, 'hours').format('HH:mm:ss');
 };
 setInterval(updateTime, 1000);

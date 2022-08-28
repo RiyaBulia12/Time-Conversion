9a0974f687f2b2ce4ee79584b0333c942c63c0b7
Apple <riyabulia12@gmail.com>
2022-08-28 20:20:30 +0530

    Update utc+1

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import './main.css'
 const moment = require('moment');
 
 const localDate = document.getElementById('localDate');
-const localTimeZone = document.getElementById('localTimeZone');
 const localTime = document.getElementById('localTime');
+const utcDate = document.getElementById('utcDate');
+const utc = document.getElementById('utc');
+const utcTime = document.getElementById('utcTime');
 
-const now = moment();
-localDate.innerHTML = now.format("ddd, MMM D YYYY");
 const updateTime = () => {
+   localDate.innerHTML = moment().format("ddd, MMM D YYYY");
+   utcDate.innerHTML = moment.utc().add(1, 'hours').format("ddd, MMM D YYYY");
    localTime.innerHTML = moment().format("HH:mm:ss")
+   utcTime.innerHTML = moment.utc().add(1, 'hours').format('HH:mm:ss');
 };
 setInterval(updateTime, 1000);
